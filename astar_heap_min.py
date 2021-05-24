8cb231e8cc5936cc081f67e210fdb9c6f8ac621d
ixtiyoruz <ixtiyoruz312@gmail.com>
2021-05-24 16:24:12 +0900

    Update astar_heap_min.py

--- astar_heap_min.py
+++ astar_heap_min.py
@@ -4,8 +4,8 @@ import cv2
 import matplotlib.pyplot as plt
 import math
 import heapq
-cell_size_orig = 1 # cell size aka each pixel size in m
-cell_size_curr = 0.1 # cell size we are going to use the algorithm for
+# cell_size_orig = 1 # cell size aka each pixel size in m
+# cell_size_curr = 0.15 # cell size we are going to use the algorithm for
 animate = False
 class Node:
     def __init__(self, x, y) -> None:
@@ -14,16 +14,20 @@ class Node:
         self.g = 99999 # initially it will be infinity
         self.h = 0
         self.f = 99999 # initially it will be infinity
+        self.prev = None
 
     def calculate_heuristics(self, end):
         self.h = self.distanceH(self, end)
-    def move(self,step,end):
+    def move(self,step,end=None):
         n = Node(self.x+step[0], self.y + step[1])
-        n.calculate_heuristics(end)        
+        if(end is not None):
+            n.calculate_heuristics(end)        
         return n
-    
+    def new(coor):
+        return Node(coor[0],  coor[1])
     def distanceH(self, n1, end):
-        return abs(n1.x - end[0]) + abs(n1.y - end[1])
+        return (abs(n1.x - end[0])+ abs(n1.y - end[1])) *100
+
     def distance(self, n1, end):
         return round(math.hypot(n1.x - end[0], n1.y - end[1]),2)
     def __eq__(self, o) -> bool:
@@ -41,7 +45,7 @@ def check_collision(node:Node, map)->bool:
         return True
     if(node.y < 0 or node.y >= map.shape[1]):
         return True
-    if(map[node.x, node.y,0] < 100):
+    if(np.any(map[node.x-2:node.x + 2, node.y-2:node.y + 2,0]< 100)):
         return True
     return False
 
@@ -52,7 +56,7 @@ def movements():
         (1, 1, 1.14),# south east
         (1, 0, 1), # south
         (1,-1, 1.14), # south west
-        (0,-1, 1), # west
+        (0,-1, 1), # westcell_size_orig
         (-1,-1, 1.14), # north west
         (-1, 0, 1), # north
         (-1, 1, 1.14) # north east
@@ -60,26 +64,25 @@ def movements():
 def calculate_index(node, shape):
     return node.x * shape[0] + node.y
 
-def djirska(map, start=(10,28), end=(270,293)):
-    costmat = np.zeros_like(map)
+def astar_heap(costmap, start, end):
+    print(start, end)
     visited = {}
     queue = {}
     snode =Node(start[0],start[1])
     snode.g = 0
-    snode.f = snode.g + snode.h
     snode.calculate_heuristics(end)
-    in_curr = calculate_index(snode, map.shape)
+    snode.f = snode.g + snode.h    
+    in_curr = calculate_index(snode, costmap.shape)
     queue[in_curr] = snode
     heap = []
     heapq.heappush(heap, (snode.f, in_curr))
-    if(map[end[0],end[1],0] < 100):
+    if(costmap[end[0],end[1],0] < 100):
         print('target has an obstacle')
-        return
-    
+        return None
     steps = 0
     while(len(queue) >0):
         steps = steps + 1
-        print('steps = ', steps)
+        # print('steps = ', steps)
         
         # search for the best node in the queue
         keys =list(queue.keys())
@@ -90,32 +93,86 @@ def djirska(map, start=(10,28), end=(270,293)):
 
         if(curr.x == end[0] and curr.y == end[1]): 
             print("found the target")
-            break
+            res = []
+            while(True):
+                res.append([curr.x, curr.y, curr.g])
+                curr = curr.prev
+                if(curr is None):
+                    break
+            # res.reverse()
+            return res
+        
 
         # find the neighbors
         for step in movements():
             adjacentnode = curr.move(step, end)
-            g = curr.g + step[2]
+            adjacentnode.prev = curr
+            g = curr.g + 100*step[2] + 100 *(255-costmap[int(adjacentnode.x), int(adjacentnode.y), 0])
 
             # if(g < adjacentnode.g):
             adjacentnode.g = g
             adjacentnode.f = adjacentnode.g + adjacentnode.h
-            ind_tmp = calculate_index(adjacentnode, map.shape)
+            ind_tmp = calculate_index(adjacentnode, costmap.shape)
             if(not (ind_tmp in visited or ind_tmp in queue) ):
-                if(not check_collision(adjacentnode, map)):
+                if(not check_collision(adjacentnode, costmap)):
                     queue[ind_tmp] = adjacentnode
                     heapq.heappush(heap, (adjacentnode.f, ind_tmp))
             
         # add the current to visited
         visited[in_curr] = curr
-        map[curr.x, curr.y] = [255,0,0]
+        # print(np.shape(costmap))
+        # costmap[curr.x, curr.y] = [127,0,127]
         
-        if(animate):
-            plt.imshow(map_new)
-            plt.waitforbuttonpress(0.1)
-            
+        # if(animate):
+        #     plt.imshow(costmap)
+        #     # plt.waitforbuttonpress(0.1)
+        #     plt.show()
+def astar_heap_find_open_place(costmap, start, end):
+    visited = {}
+    queue = {}
+    snode =Node(start[0],start[1])
+    snode.g = 0
+    # snode.calculate_heuristics(end)
+    snode.f = snode.g+snode.h 
+    in_curr = calculate_index(snode, costmap.shape)
+    print(in_curr)
+    queue[in_curr] = snode
+    heap = []
+    heapq.heappush(heap, (snode.f, in_curr))
     
-def reconstruct_map(map_img):
+    steps = 0
+    while(len(queue) >0):
+        steps = steps + 1
+        # print('steps = ', steps)
+        
+        # search for the best node in the queue
+        keys =list(queue.keys())
+        _,in_curr = heapq.heappop(heap)
+
+        curr = queue[in_curr]
+        del queue[in_curr]
+        
+        # find endnode
+        if(not check_collision(curr, costmap)): 
+            print("found the target")
+            return [curr.x, curr.y, math.atan2(curr.y, curr.x)]
+
+        # find the neighbors
+        for step in movements():
+            adjacentnode = curr.move(step, None)
+            adjacentnode.prev = curr
+            g = curr.g + step[2]
+            
+            adjacentnode.g = g
+            adjacentnode.f = adjacentnode.g#+ adjacentnode.h
+            ind_tmp = calculate_index(adjacentnode, costmap.shape)
+            if(not (ind_tmp in visited or ind_tmp in queue) ):
+                queue[ind_tmp] = adjacentnode
+                heapq.heappush(heap, (adjacentnode.f, ind_tmp))
+            
+        visited[in_curr] = curr
+    
+def reconstruct_map(map_img, cell_size_curr = 1, cell_size_orig = 1.0):
     w,h,_ = map_img.shape
     wn,hn = int(w * cell_size_orig/cell_size_curr),int(h * cell_size_orig/cell_size_curr)
     map_new = cv2.resize(map_img,(wn, hn), interpolation = cv2.INTER_NEAREST)
@@ -126,7 +183,10 @@ if __name__ == "__main__":
     map_img = cv2.imread(map_path, -1)
     map_new = reconstruct_map(map_img)
     map_in = map_new
-    djirska(map_in)
+    import time 
+    start = time.time()
+    astar_heap(map_in, (10,10),(30, 25))
+    print(time.time() - start)
     plt.imshow(map_new)
     plt.show()
     # plt.waitforbuttonpress()
